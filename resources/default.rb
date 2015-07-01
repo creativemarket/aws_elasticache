54ae1268aaf5948be04ef6f68f4038c817353c94
Jeremy Koerber <jeremy@creativemarket.com>
2015-07-01 09:59:32 -0700

    fix accidental regression on aws key reqs

--- resources/default.rb
+++ resources/default.rb
@@ -1,8 +1,8 @@
 actions :create, :update, :delete
 default_action :create
 
-attribute :aws_access_key, kind_of: String, required: true
-attribute :aws_secret_access_key, kind_of: String, required: true
+attribute :aws_access_key, kind_of: String
+attribute :aws_secret_access_key, kind_of: String
 attribute :region, kind_of: String, required: true
 attribute :cache_cluster_id, kind_of: String, name_attribute: true
 attribute :num_cache_nodes, kind_of: Integer, required: true
@@ -14,4 +14,4 @@ attribute :cache_subnet_group_name, kind_of: String, required: true
 attribute :security_group_ids, kind_of: Array, required: true
 attribute :port, kind_of: Integer, required: true
 attribute :auto_minor_version_upgrade, kind_of: [TrueClass, FalseClass], default: false
-attribute :store_in_etcd, kind_of: [TrueClass, FalseClass], default:false
+attribute :store_in_etcd, kind_of: [TrueClass, FalseClass], default: false
